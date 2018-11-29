442c688bfd9658d8f5d154b9ac04c6aaad059d80
ashwinbhide13 <43821970+ashwinbhide13@users.noreply.github.com>
2018-11-28 19:59:05 -0500

    Add files via upload

---
+++ findPeak.m
@@ -0,0 +1,14 @@
+function f = findPeak(x, y)
+    ans = []
+    offset = 5
+    for i = 1 : length(y)-offset
+        j = i+offset-1;
+        window = y(i:j);
+        %disp(window);
+        avg = mean(window);
+        if y(j+1) > avg
+            [ans, j+1];
+        end
+    end
+    f = ans
+end
